2aa4131a86bdada1371e673312eb41dbabe328d7
tangzhihong <tangzhihong@ultrapower.com.cn>
2026-05-08 14:52:12 +0800

    fix: docs antd dark theme sync with vitepress

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,23 @@
+import { theme } from 'ant-design-vue';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
+import { useData } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
+import { computed, h } from 'vue';
+import { ConfigProvider } from 'ant-design-vue';
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.use(Antd);
+  },
+  Layout() {
+    const { isDark } = useData();
+    const antdTheme = computed(() => ({
+      algorithm: isDark.value ? theme.darkAlgorithm : theme.defaultAlgorithm,
+    }));
+    return h(ConfigProvider, { theme: antdTheme.value }, () =>
+      h(DefaultTheme.Layout),
+    );
   },
 };
